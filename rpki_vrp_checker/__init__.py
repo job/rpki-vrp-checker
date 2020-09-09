153dd30cf207e0d134608415684596405990ab55
Job Snijders <job@sobornost.net>
2020-09-09 12:10:51 +0000

    add init file which has version

---
+++ rpki_vrp_checker/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.1"
+__author__ = "Job Snijders"
+__author_email__ = "job@ntt.net"
+__copyright__ = "Copyright 2020, Job Snijders"
+__license__ = "BSD 2-Clause"
+__status__ = "Development"
+__url__ = "https://github.com/job/rpki-vrp-checker"
+
+from .rpki_vrp_checker import vrp_checker
